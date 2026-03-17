891650af4ffc621e34617499636dcf4f1d87cb1a
s-afrid <afrid0052@gmail.com>
2026-03-17 12:15:02 +0530

    added basic structure to navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import Link from "next/link"
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
+    <nav className='h-16 bg-purple-700 flex justify-between px-3 items-center text-white'>
+        <div className="logo font-bold text-lg">
+            URL Short
+        </div>
+        <ul className='flex justify-center items-center gap-4'>
+            <Link href='/'><li>Home</li></Link>
+            <Link href='/about'><li>About</li></Link>       
+            <Link href='/generate'><li>Shorten</li></Link>      
+            <Link href='/contact'><li>Contact Us</li></Link>
+            <li className="flex gap-3">
+                <Link href="/generate"><button className="bg-purple-500 shadow-lg px-3 py-1 font-bold rounded-lg">Try Now</button></Link>
+                <Link href="/github"><button className="bg-purple-500 shadow-lg px-3 py-1 font-bold rounded-lg">GitHub</button></Link>   
+            </li>
+        </ul>
+    </nav>
   )
 }
 
