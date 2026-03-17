44705988587ba28b9e97960b8e62919be0194b49
s-afrid <afrid0052@gmail.com>
2026-03-17 16:54:57 +0530

    section 1 on main page

--- app/page.js
+++ app/page.js
@@ -2,6 +2,21 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>Home page</div>
+    <main>
+      <section className="grid grid-col-2 h-[50vh]">
+        <div className="bg-green-200">
+          <p>
+          The best URL shortener in the Market
+          </p>
+          <p>
+            We are the most straightforward URL in the world
+          </p>
+        </div>
+        <div className="bg-red-500 flex justify-start relative">
+        <Image alt="an Image of a vector" src={"/vector.jpg"} fill={true} />
+        </div>
+
+      </section>
+    </main>
   );
 }
