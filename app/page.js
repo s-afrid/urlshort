644f3826d036828d10cea8f198b538f6b8fece50
s-afrid <afrid0052@gmail.com>
2026-03-18 12:19:37 +0530

    basic styling of section 1

--- app/page.js
+++ app/page.js
@@ -1,19 +1,28 @@
 import Image from "next/image";
+import localFont from 'next/font/local';
+
+const poppins = localFont(
+  {
+    src: './fonts/Poppins-ExtraBold.ttf',
+    variable: '--font-poppins',
+    weight: "100 900"
+  }
+)
 
 export default function Home() {
   return (
-    <main>
-      <section className="grid grid-col-2 h-[50vh]">
-        <div className="bg-green-200">
-          <p>
+    <main className="bg-purple-100">
+      <section className="grid grid-cols-2 h-[50vh]">
+        <div className="flex flex-col gap-4 items-center justify-center">
+          <p className={`text-3xl font-bold ${poppins.className}`}>
           The best URL shortener in the Market
           </p>
-          <p>
-            We are the most straightforward URL in the world
+          <p className="px-56">
+            We are the most straightforward URL in the world. Most of the URL shorteners will track you or ask you give your details for login. We understand your needs and hence we have created this URL shortener
           </p>
         </div>
-        <div className="bg-red-500 flex justify-start relative">
-        <Image alt="an Image of a vector" src={"/vector.jpg"} fill={true} />
+        <div className="flex justify-start relative">
+        <Image className="mix-blend-darken" alt="an Image of a vector" src={"/vector.jpg"} fill={true} />
         </div>
 
       </section>
