bf0bbf30673e63277016bb43eb087d961ad14903
s-afrid <afrid0052@gmail.com>
2026-03-22 09:35:59 +0530

    set state variables

--- app/shorten/page.js
+++ app/shorten/page.js
@@ -7,24 +7,24 @@ const Shorten = () => {
     const [shorturl, setshorturl] = useState("")
     const [generated, setgenerated] = useState("")
 
-    const handleChange = ()=>{
-
-    }
+    
   return (
     <div className='mx-auto max-w-lg bg-purple-100 my-16 p-8 rounded-lg flex flex-col gap-4'>
       <h1 className='font-bold text-2xl'>Generate your short URLs</h1>
       <div className='flex flex-col gap-4'>
         <input 
             type="text"
+            value={url}
             className='px-4 py-2 focus:outline-purple-600 rounded-md bg-white' 
             placeholder='Enter your URL' 
-            onChange={handleChange} />
+            onChange={e => {seturl(e.target.value)}} />
         <input 
             type="text"
+            value={shorturl}
             className='px-4 py-2 focus:outline-purple-600 rounded-md bg-white' 
             placeholder='Enter your preferred short URL' 
-            onChange={handleChange} />
-        <button className="bg-purple-500 shadow-lg px-3 py-1 font-bold rounded-lg text-white my-2">Generate</button>
+            onChange={e => {setshorturl(e.target.value)}} />
+        <button className="bg-purple-500 shadow-lg px-3 py-1 my-3 font-bold rounded-lg text-white my-2">Generate</button>
       </div>
     </div>
   )
